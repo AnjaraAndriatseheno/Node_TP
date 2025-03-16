1e9caa7966553845b92f0214cfef5e14b496db67
cpc <ilyaselaz2004@gmail.com>
2025-03-16 22:21:47 +0100

    base de données et message

--- front-chatbot/Backend/src/routes/logsRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { deleteLogs, getLogs } from '../services/logsServices.js';
-
-const router = express.Router();
-
-router.get('/logs', getLogs)
-router.delete('/logs/:id', deleteLogs)
-
-export default router;
